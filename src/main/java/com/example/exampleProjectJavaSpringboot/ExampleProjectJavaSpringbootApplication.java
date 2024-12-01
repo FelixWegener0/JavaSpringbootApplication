45761fece1779da06e7f3c7dfd61ff4edeba85ee
Felix Wegener <Felix.Wegener05@t-online.de>
2024-12-01 17:41:36 +0100

    add scheduling

--- src/main/java/com/example/exampleProjectJavaSpringboot/ExampleProjectJavaSpringbootApplication.java
+++ src/main/java/com/example/exampleProjectJavaSpringboot/ExampleProjectJavaSpringbootApplication.java
@@ -1,12 +1,35 @@
 package com.example.exampleProjectJavaSpringboot;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Configuration
+@EnableScheduling
 @SpringBootApplication
 public class ExampleProjectJavaSpringbootApplication {
 
-	public static void main(String[] args) {
+	public static void initFireBaseApp() throws IOException {
+		FileInputStream serviceAccount = new FileInputStream("hygrometer-8de9a-firebase-adminsdk-g7iae-b3f09dcbe2.json");
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+		FirebaseApp.initializeApp(options);
+		System.out.println("init Firebase for cloudMessaging");
+	}
+
+	public static void main(String[] args) throws IOException {
+		initFireBaseApp();
 		SpringApplication.run(ExampleProjectJavaSpringbootApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/exampleProjectJavaSpringboot/Scheduling/Scheduling.java
@@ -0,0 +1,14 @@
+package com.example.exampleProjectJavaSpringboot.Scheduling;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduling {
+
+    @Scheduled(fixedDelay = 1000)
+    public void scheduleFixedDelayTask() {
+        System.out.println("Fixed delay task - " + System.currentTimeMillis() / 1000);
+    }
+
+}
