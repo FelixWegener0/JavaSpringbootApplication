e779dd844fbd686419c33872b96291aadd9d2b0c
Felix Wegener <Felix.Wegener05@t-online.de>
2024-12-01 13:16:09 +0100

    init commit

---
+++ src/main/java/com/example/exampleProjectJavaSpringboot/ExampleProjectJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleProjectJavaSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleProjectJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleProjectJavaSpringbootApplication.class, args);
+	}
+
+}
